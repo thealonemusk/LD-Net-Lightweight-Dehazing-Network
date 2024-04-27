05df6bfe80edbce8c258f7bdec3a4eee83d3fde5
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2024-04-27 18:44:42 +0530

    improved the api code

--- backend/main.py
+++ backend/main.py
@@ -160,6 +160,7 @@ async def dehaze_image(image: UploadFile = File(...)):
         except Exception as e:
             raise Exception(400, str(e))
         
+
         train_hazy_loader = torch.utils.data.DataLoader(dataset=input_image, batch_size=1, shuffle=False)
 
         dehazed_output = []
@@ -192,12 +193,7 @@ async def dehaze_image(image: UploadFile = File(...)):
         rotated_image = np.rot90(X_dehazed.squeeze(), k=-1) 
         mirror_image_horizontal = np.flip(rotated_image, axis=1)
 
-        plt.subplot(133)
-        plt.title('Dehazed Image')
-        plt.imshow(mirror_image_horizontal)  # Squeeze to remove batch dimension if present
-        plt.axis('off')
-
-        plt.show()
+        mirror_image_horizontal = (mirror_image_horizontal * 255).clip(0, 255).astype(np.uint8)
         # Return the output image as a response stream
         # Convert the image array to bytes
         # Save the image to a temporary file
